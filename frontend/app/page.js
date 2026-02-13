240f9f2b0be097733b926cb06b71cb5a423a5dbf
Huzaifa25a <pachisahuzaifa@gmail.com>
2026-02-13 19:38:57 +0400

    minor bug fixes

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -13,7 +13,7 @@ export default function LoginPage() {
   const router = useRouter();
 
   const handleLogin = async (e) => {
-    const res = await fetch("http://localhost:3000/api/auth/login", {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
@@ -40,7 +40,7 @@ export default function LoginPage() {
     if(!emailPattern.test(email)){
         return alert('Email is not valid!');
     }
-    const res = await fetch("http://localhost:3000/api/auth/register", {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name, email, password }),
